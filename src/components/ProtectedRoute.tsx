a625b79985e80a3fc5918848a41cd1e7ce033b0a
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2021-07-24 21:06:21 -0700

    cleanup user model, dont use uuid

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,8 +1,8 @@
-import { Spinner } from "@chakra-ui/react";
 import React from "react";
 import { Redirect, Route, RouteProps } from "react-router-dom";
-import useUser from "../hooks/useUser";
 import { Container } from "./Container";
+import { Spinner } from "@chakra-ui/react";
+import useUser from "../hooks/useUser";
 
 export const ProtectedRoute: React.FC<RouteProps> = ({ children, ...rest }) => {
   const [user, , loading] = useUser();
